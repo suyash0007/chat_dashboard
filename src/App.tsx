bc59a2d8b896c060418d25e598ae43122eb32947
Suyash Tiwari <tiwarisuyash40@gmail.com>
2024-10-06 13:35:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import SideBar from './components/SideBar';
 import ChatDashboard from './components/ChatDashboard';
 import { ChatSessionProvider } from './context/ChatSessionContext';
